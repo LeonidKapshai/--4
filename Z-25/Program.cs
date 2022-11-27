61a653b87904cdd3472b2122e2709c188f9796ab
Kapshai <leonid.kapshai@gmail.com>
2022-11-27 21:37:53 +0300

    Первая задача

---
+++ Z-25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите чило:");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа:");
+var num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Getsum(num2));
+
+int Getsum (int num) 
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+{
+    sum = sum * num1;
+}
+return sum;
+}
